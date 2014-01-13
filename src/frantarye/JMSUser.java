927a38af008f6bfbabf4206ff5da8836d6504fcf
gye-tgm <gye@student.tgm.ac.at>
2014-01-13 12:32:38 +0100

    Implementation of the JMSUser and the Syntaxchecker added.

--- src/frantarye/JMSUser.java
+++ src/frantarye/JMSUser.java
@@ -1,29 +1,107 @@
 package frantarye;
 
-import org.apache.activemq.ActiveMQConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.jms.JMSException;
 
 /**
  * @author Gary Ye
- * @version 13.01.2013
- * Objects of this 
+ * @version 13.01.2013 The user class runs the logic of this application. It
+ *          reads the message from the standard input and handles it
+ *          correspondingly. It also runs a thread in the background, that
+ *          receives the messages, which will be printed in the standard output.
  */
 public class JMSUser extends Thread {
 	private JMSChat chat;
 	private JMSMail mail;
-	
+
 	private String ip;
 	private String username;
-	
+
+	/**
+	 * Constructs a new user.
+	 * 
+	 * @param ip_message_broker
+	 *            the IP address of the message broker
+	 * @param username
+	 *            the user name of this user
+	 * @param subject
+	 *            the subject of the chat room
+	 */
 	public JMSUser(String ip_message_broker, String username, String subject) {
-		
+		// Own attributes
+		this.username = username;
+		// Chat message
+		String url = String.format("tcp://%s:61616", ip_message_broker);
+		chat = new JMSChat(subject, url);
+
+		// Mail Box
+		try {
+			mail = new JMSMail(Inet4Address.getLocalHost().getHostAddress(),
+					url);
+		} catch (UnknownHostException e) {
+			System.err.println("The localhost is unknown.");
+			System.exit(1);
+		}
 	}
-	
+
+	/**
+	 * Reads the commands from the standard input and handles it
+	 * correspondingly.
+	 */
 	@Override
 	public void run() {
-		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String inputLine;
+
+		chat.run(); // Start listening
+
+		try {
+			while ((inputLine = in.readLine()) != null) {
+				SyntaxChecker.InputType type = SyntaxChecker
+						.checkInput(inputLine);
+				switch (type) {
+				case ERROR:
+					System.err.println("Unknown error\n");
+					break;
+				case MAIL_SEND:
+					String[] split = inputLine.split(" ");
+					// MAIL mailbox, message is the rest of the line
+					mail.sendMail(split[1],
+							Arrays.copyOfRange(split, 2, split.length)
+									.toString());
+					break;
+				case MAILBOX:
+					ArrayList<String> mails = mail.retrieveMails();
+					for (String m : mails)
+						System.out.println(m);
+					break;
+				case MESSAGE_SEND:
+					try {
+						chat.sendMessage(username, ip, inputLine);
+					} catch (JMSException e) {
+						System.err.println(JMSChat.JMS_ERROR + "\n" + e.getMessage());
+					}
+					break;
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("IOException occured!\n");
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+		if (args.length != 3) {
+			System.err
+					.println("usage: vsdbchat <ip_message_broker> <username> <chatroom>");
+		} else {
+			new JMSUser(args[0], args[1], args[2]).start();
+		}
 	}
 }
--- src/frantarye/SyntaxChecker.java
+++ src/frantarye/SyntaxChecker.java
@@ -1,21 +1,50 @@
 package frantarye;
 
+/**
+ * @author Gary Ye
+ * @version 13.01.2013 This class offers a syntax check method, which can be
+ *          called statically.
+ */
 public class SyntaxChecker {
 	/*
-	private static final String ERROR_1 = "";
-	private static final String ERROR_2 = "";
-	private static final String ERROR_3 = "";
-	private static final String ERROR_4 = "";
-	*/
-	
+	 * private static final String ERROR_1 = ""; private static final String
+	 * ERROR_2 = ""; private static final String ERROR_3 = ""; private static
+	 * final String ERROR_4 = "";
+	 */
+
 	public enum InputType {
-		MESSAGE_SEND,
-		MAIL_SEND,
-		MAILBOX,
-		ERROR
+		MESSAGE_SEND, MAIL_SEND, MAILBOX, ERROR
 	}
-	
+
+	/**
+	 * Checks if the input of the user is valid and detects the type of the
+	 * command.
+	 * 
+	 * @param input
+	 *            the input string
+	 * @return the type of the command
+	 */
 	public static InputType checkInput(String input) {
-		return InputType.ERROR;
+		if (input.isEmpty())
+			return InputType.ERROR;
+		String[] split = input.split(" ");
+		switch (split[0]) {
+		case "MAIL":
+			if (split.length >= 3)
+				return InputType.MAIL_SEND;
+			else {
+				System.err.println("usage: MAIL <ip_partner> <nachricht>");
+				return InputType.ERROR;
+			}
+		case "MAILBOX":
+			if (split.length == 1)
+				return InputType.MAILBOX;
+			else {
+				System.err.println("usage: MAILBOX");
+				return InputType.ERROR;
+			}
+		}
+		// Everything else will be recognized as a normal chat message
+		return InputType.MESSAGE_SEND;
 	}
 }
