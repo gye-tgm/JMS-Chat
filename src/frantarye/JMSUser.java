f8a4c85b75f895dc9645389722e7b90bcc3b3393
gye-tgm <gye@student.tgm.ac.at>
2014-01-17 15:36:26 +0100

    Adjusted the build file

--- src/frantarye/JMSUser.java
+++ src/frantarye/JMSUser.java
@@ -3,15 +3,12 @@ package frantarye;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Enumeration;
-import java.util.regex.Pattern;
-
 import javax.jms.JMSException;
 
 /**
